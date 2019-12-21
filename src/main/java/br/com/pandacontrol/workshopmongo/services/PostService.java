d9f62a619cc08b91c59a84d4edb95eb5ab2a1865
jefersonfsilva <jefersonfs@gmail.com>
2019-12-21 17:24:20 -0300

    Obtendo um post por id

---
+++ src/main/java/br/com/pandacontrol/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package br.com.pandacontrol.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.pandacontrol.workshopmongo.domain.Post;
+import br.com.pandacontrol.workshopmongo.repository.PostRepository;
+import br.com.pandacontrol.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
